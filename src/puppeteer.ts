e15b69b14b24ef7e9d60a6c39475a63b7249b3bf
Jason McGhee <mcghee.j@gmail.com>
2023-04-08 17:56:45 -0700

    slow down question asking

--- src/puppeteer.ts
+++ src/puppeteer.ts
@@ -6,12 +6,20 @@ export class Puppeteer {
   page: Page;
 
   async init() {
-    this.browser = await puppeteer.launch({ headless: false });
+    this.browser = await puppeteer.launch();
     this.page = await this.browser.newPage();
 
     await this.page.goto("http://localhost:5173/documents/hello");
     await this.page.setViewport({ width: 1080, height: 1024 });
     await this.page.waitForSelector(".mantine-RichTextEditor-content");
+
+    this.page.on('console', (message) => {
+      if (message.type() === 'log') {
+        console.log(`Console log: ${message.text()}`);
+      } else if (message.type() === 'error') {
+        console.error(`Console error: ${message.text()}`);
+      }
+    });
 
     await this.page.evaluate(async () => {
       await (window as any).editor
@@ -28,11 +36,24 @@ export class Puppeteer {
 
   async insertContent(content: string) {
     await this.page.evaluate(async (content) => {
-      await (window as any).editor
+      let chain = await (window as any).editor
         .chain()
         .focus("end")
-        .insertContent(`<p>${content}</p>`)
+        .insertContent(`<p></p>`)
         .run();
+
+      const letters = content.split("")
+      for (let i = 0; i < letters.length; i++) {
+        await new Promise((resolve) => {
+          setTimeout(() => {
+            (window as any).editor
+              .chain()
+              .insertContent(letters[i])
+              .run();
+            resolve(true);
+          }, 10)
+        });
+      }
     }, content);
   }
 
